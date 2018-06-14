9599e8f3e50f95243279496224f62ed22e4daad4
Jonathan Davis <cartman61616@gmail.com>
2018-06-14 19:28:05 -0400

    Updating project 1 to meet PEP standards

--- project_1/entertainment_center.py
+++ project_1/entertainment_center.py
@@ -1,31 +1,34 @@
-import media, fresh_tomatoes
+import media
+import fresh_tomatoes
 
-#movie objects
+""" Class that holds the movie objects for the fresh tomatoes site
+After creating movie objects it takes those objects into a list.
+That list is passed into the fresh_tomatoes file"""
+
 avengers = media.Movie("The Avengers",
-                        "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
-                        "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+"https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
+"https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
 super_troopers = media.Movie("Super Troopers", 
-                             "https://upload.wikimedia.org/wikipedia/en/1/19/Supertrooper.jpg",
-                             "https://www.youtube.com/watch?v=TKzD0z8Jv9I")
+"https://upload.wikimedia.org/wikipedia/en/1/19/Supertrooper.jpg",
+"https://www.youtube.com/watch?v=TKzD0z8Jv9I")
 
 incredibles = media.Movie("The Incredibles", 
-                            "https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg",
-                            "https://www.youtube.com/watch?v=eZbzbC9285I")
+"https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg",
+"https://www.youtube.com/watch?v=eZbzbC9285I")
 
 mallrats = media.Movie("Mallrats", 
-                        "https://upload.wikimedia.org/wikipedia/en/9/96/Mallrats.jpg",
-                        "https://www.youtube.com/watch?v=eOd5zJLsZEc")
+"https://upload.wikimedia.org/wikipedia/en/9/96/Mallrats.jpg",
+"https://www.youtube.com/watch?v=eOd5zJLsZEc")
 
 deadpool = media.Movie("Deadpool", 
-                        "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
-                        "https://www.youtube.com/watch?v=Xithigfg7dA")
+"https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
+"https://www.youtube.com/watch?v=Xithigfg7dA")
 
 deadpool_2 = media.Movie("Deadpool 2", 
-                        "https://upload.wikimedia.org/wikipedia/en/c/cf/Deadpool_2_poster.jpg",
-                        "https://www.youtube.com/watch?v=D86RtevtfrA")
-#list of the movie objects
+"https://upload.wikimedia.org/wikipedia/en/c/cf/Deadpool_2_poster.jpg",
+"https://www.youtube.com/watch?v=D86RtevtfrA")
+
 movies = [avengers, super_troopers, incredibles, mallrats, deadpool, deadpool_2]
 
-#call fresh tomatoes and load the list of movies 
 fresh_tomatoes.open_movies_page(movies)
--- project_1/media.py
+++ project_1/media.py
@@ -1,11 +1,19 @@
 import webbrowser
 
-#constructor to hold movie information such as movie title, poster, and trailer on youtube
+"""constructor to hold movie information
+such as movie title, poster, and trailer on youtube
+
+Args: 
+    title(str): movie title
+    poster_image_url: url for the poster image
+    trailer_youtube_url: url for the trailer on youtube
+"""
+
 class Movie():
-    def __init__(self, movie_title, poster_image, trailer_youtube) :
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
-        self.trailer_youtube_url =  trailer_youtube
-    
+        self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
