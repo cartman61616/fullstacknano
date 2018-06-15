0d7fa5fd9a01e32fcb06a911542292db4b3bf62c
Jonathan Davis <cartman61616@gmail.com>
2018-06-15 19:54:15 -0400

    Updating project 1 code with feedback from code review

--- project_1/entertainment_center.py
+++ project_1/entertainment_center.py
@@ -6,29 +6,34 @@ After creating movie objects it takes those objects into a list.
 That list is passed into the fresh_tomatoes file"""
 
 avengers = media.Movie("The Avengers",
-"https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
-"https://www.youtube.com/watch?v=eOrNdBpGMv8")
+                       "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",  # NOQA
+                       "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
-super_troopers = media.Movie("Super Troopers", 
-"https://upload.wikimedia.org/wikipedia/en/1/19/Supertrooper.jpg",
-"https://www.youtube.com/watch?v=TKzD0z8Jv9I")
+super_troopers = media.Movie("Super Troopers",
+                             "https://upload.wikimedia.org/wikipedia/en/1/19/Supertrooper.jpg",  # NOQA
+                             "https://www.youtube.com/watch?v=TKzD0z8Jv9I")
 
-incredibles = media.Movie("The Incredibles", 
-"https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg",
-"https://www.youtube.com/watch?v=eZbzbC9285I")
+incredibles = media.Movie("The Incredibles",
+                          "https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg",  # NOQA
+                          "https://www.youtube.com/watch?v=eZbzbC9285I")
 
-mallrats = media.Movie("Mallrats", 
-"https://upload.wikimedia.org/wikipedia/en/9/96/Mallrats.jpg",
-"https://www.youtube.com/watch?v=eOd5zJLsZEc")
+mallrats = media.Movie("Mallrats",
+                       "https://upload.wikimedia.org/wikipedia/en/9/96/Mallrats.jpg",  # NOQA
+                       "https://www.youtube.com/watch?v=eOd5zJLsZEc")
 
-deadpool = media.Movie("Deadpool", 
-"https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
-"https://www.youtube.com/watch?v=Xithigfg7dA")
+deadpool = media.Movie("Deadpool",
+                       "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",  # NOQA
+                       "https://www.youtube.com/watch?v=Xithigfg7dA")
 
-deadpool_2 = media.Movie("Deadpool 2", 
-"https://upload.wikimedia.org/wikipedia/en/c/cf/Deadpool_2_poster.jpg",
-"https://www.youtube.com/watch?v=D86RtevtfrA")
+deadpool_2 = media.Movie("Deadpool 2",
+                         "https://upload.wikimedia.org/wikipedia/en/c/cf/Deadpool_2_poster.jpg",  # NOQA
+                         "https://www.youtube.com/watch?v=D86RtevtfrA")
 
-movies = [avengers, super_troopers, incredibles, mallrats, deadpool, deadpool_2]
+movies = [avengers,
+          super_troopers,
+          incredibles,
+          mallrats,
+          deadpool,
+          deadpool_2]
 
 fresh_tomatoes.open_movies_page(movies)
